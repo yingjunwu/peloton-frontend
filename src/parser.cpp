b2d3e64922436d54dca617a95cf714031fcd2b00
jarulraj <jarulraj@cs.cmu.edu>
2016-04-15 22:07:01 -0400

    Checkpoint

---
+++ src/parser.cpp
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// parser.cpp
+//
+// Identification: src/parser.cpp
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+int main() {
+  return 0;
+}
--- tests/test-driver.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# Extract the file name of the tes executable
-name=${1##*/}
-echo $name
-echo $pwd
-valgrind --error-exitcode=1 --leak-check=full --quiet --track-origins=yes --suppressions=./valgrind.supp --xml=yes --xml-file=valgrind_${name}.xml --log-file=valgrind_${name}.log -v ./${name} --gtest_output=xml:"unit_"${name}".xml"
-#exec $1 --gtest_output="xml:"$1".xml"
